507127f8bb31de74f94b14878f8c85b61b844aa0
Arnd Bergmann <arnd@arndb.de>
2017-01-11 15:56:44 +0100

    BACKPORT: lkdtm: hide stack overflow warning for corrupt-stack test
    After the latest change to make sure the compiler actually does a memset,
    it is now smart enough to flag the stack overflow at compile time,
    at least with gcc-7.0:
    
    drivers/misc/lkdtm.c: In function 'lkdtm_CORRUPT_STACK':
    drivers/misc/lkdtm.c:88:144: warning: 'memset' writing 64 bytes into a region of size 8 overflows the destination [-Wstringop-overflow=]
    
    To outsmart the compiler again, this moves the memset into a noinline
    function where (for now) it doesn't see that we intentionally write
    broken code here.
    
    Fixes: c55d240003ae ("lkdtm: Prevent the compiler from optimising lkdtm_CORRUPT_STACK()")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://git.kernel.org/linus/7a11a1d1b58873b2e5a6922dcdc23b6b339b14ba
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    # Conflicts:
    #	drivers/misc/lkdtm.c

--- drivers/misc/lkdtm.c
+++ drivers/misc/lkdtm.c
@@ -353,8 +353,7 @@ static noinline void corrupt_stack(void)
 	/* Use default char array length that triggers stack protection. */
 	char data[8];
 	__lkdtm_CORRUPT_STACK(&data);
-	
-	memset((void *)data, 'a', 64);
+
 	pr_info("Corrupted stack with '%16s'...\n", data);
 }
 
